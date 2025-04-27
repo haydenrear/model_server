da3d12b41698de48938c95d90adc84a1502977f5
hayden.rear <hayden.rear@gmail.com>
2025-04-27 13:37:24 -0400

    sanitize json for java

--- src/model_server/main/model_server_main.py
+++ src/model_server/main/model_server_main.py
@@ -1,14 +1,7 @@
-import os
-
 from model_server.train_conn.runner import Run
-from python_di.inject.context_builder.injection_context import InjectionContext
 
+from python_di.configs.app import boot_application
 
-# TODO: split into main method @python_di_application.
-
-inject_ctx = InjectionContext()
-env = inject_ctx.initialize_env()
-
-to_scan = os.path.dirname(os.path.dirname(__file__))
-inject_ctx.build_context(parent_sources={to_scan}, source_directory=os.path.dirname(to_scan))
-inject_ctx.ctx.get_interface(Run)
+@boot_application(root_dir_cls=Run)
+class ModelServerApplication:
+    pass
